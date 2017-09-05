1f84db81bccf659cd101c8b9e6d5d88ed685b472
zhubanRuban <30699069+zhubanRuban@users.noreply.github.com>
2017-09-05 18:49:09 +0300

    Update bashrc_custom.sh

--- bashrc_custom.sh
+++ bashrc_custom.sh
@@ -2,7 +2,7 @@
 ################################################
 # https://github.com/zhubanRuban/cygwin-extras #
 # Command prompt options
-PS1='\[\e]0;\h\a\]\u\e[1;30m@\e[32m\h\e[0m \e[1;30m[\e[0m\w\e[1;30m]\e[0m\$ '
+PS1='\[\e]0;\h\a\]\u\[\e[1;30m\]@\[\e[32m\]\h \[\e[30m\][\[\e[0m\]\w\[\e[1;30m\]]\[\e[0m\]\$ '
 # Aliases
 alias ll='ls -lah --group-directories-first --color=auto'
 alias grep='grep --color=auto'
