4b06a4543fcec5bbb79ab056e750f36203b3bc56
zhubanRuban <30699069+zhubanRuban@users.noreply.github.com>
2017-09-05 17:32:44 +0300

    Update bashrc_custom.sh

--- bashrc_custom.sh
+++ bashrc_custom.sh
@@ -4,7 +4,7 @@
 # Command prompt options
 PS1='\[\e]0;\h\a\]\u\e[1;30m@\e[32m\h\e[0m \e[1;30m[\e[0m\w\e[1;30m]\e[0m\$ '
 # Aliases
-alias ll='ls --color=auto -lah'
+alias ll='ls -lah --group-directories-first --color=auto'
 alias grep='grep --color=auto'
 alias egrep='egrep --color=auto'
 ################################################
