441d4a7a507787d545b4cfdd36bb3e7a00ebb15a
zhubanRuban <30699069+zhubanRuban@users.noreply.github.com>
2019-09-06 02:24:51 +0300

    Update and rename inputrc_custom_bind.sh to inputrc_custom_bind_install.sh

--- inputrc_custom_bind_install.sh
+++ inputrc_custom_bind_install.sh
@@ -1,4 +1,5 @@
 SCRIPTURL=https://github.com/zhubanRuban/cygwin-extras/raw/master/inputrc_custom_bind
 PATHTOINSTALL=/etc/profile.d/inputrc.sh
+
 echo Installing custom .inputrc via bind
 wget -nv --show-progress -O "$PATHTOINSTALL" "$SCRIPTURL"
