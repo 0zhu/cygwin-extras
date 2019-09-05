8f96317f066150eb13e942bd2e9398dcf3bbf2e8
zhubanRuban <30699069+zhubanRuban@users.noreply.github.com>
2019-09-06 02:01:00 +0300

    Create inputrc_custom_bind.sh

---
+++ inputrc_custom_bind.sh
@@ -0,0 +1,4 @@
+SCRIPTURL=https://github.com/zhubanRuban/cygwin-extras/raw/master/inputrc_custom_bind
+PATHTOINSTALL=/etc/profile.d/inputrc.sh
+echo Installing custom .inputrc via bind
+wget -nv --show-progress -O "$PATHTOINSTALL" "$SCRIPTURL"
