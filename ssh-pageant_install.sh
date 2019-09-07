6e3dbd48cd9f7475e1d4ae311f61af9b1a8778f6
zhubanRuban <30699069+zhubanRuban@users.noreply.github.com>
2019-09-07 15:42:17 +0300

    Update ssh-pageant_install.sh

--- ssh-pageant_install.sh
+++ ssh-pageant_install.sh
@@ -1,11 +1,17 @@
-command -v ssh >/dev/null 2>&1 || exit
-command -v ssh >/dev/null 2>&1 || apt-cyg install ssh-pageant
+#!/bin/bash
+PATH=/usr/local/bin:/usr/bin
+
+command -v ssh >/dev/null 2>&1 || { echo ssh is not installed, ssh-pageant implementation is not required; exit 1; }
+
+echo Installing ssh-pageant https://github.com/cuviper/ssh-pageant
+apt-cyg install ssh-pageant
 
 PATHTOINSTALL=/etc/profile.d/ssh-pageant.sh
 
-echo Installing ssh-pageant https://github.com/cuviper/ssh-pageant#options
-echo eval '$(/usr/bin/ssh-pageant -r -a "$HOME/.ssh/.ssh-pageant")' > $PATHTOINSTALL
+echo Applying ssh-pageant config https://github.com/cuviper/ssh-pageant#options to $PATHTOINSTALL
+echo eval '$(/usr/bin/ssh-pageant -r -a "$HOME/.ssh/.ssh-pageant")' > $PATHTOINSTALL || { echo Error while applying ssh-pageant config; exit 1; }
 
 # remove previous possible implementations
 rm -f /opt/ssh-agent-tweak
-sed -i '/\/opt\/ssh-agent-tweak/d' ~/.bashrc
+sed -i '/\/opt\/ssh-agent-tweak/d' ~/.bashrc >/dev/null 2>&1
+exit 0
