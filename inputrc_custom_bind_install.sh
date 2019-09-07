a988fa336fe48d0da9c82d6e227ce071759ea728
zhubanRuban <30699069+zhubanRuban@users.noreply.github.com>
2019-09-07 15:44:40 +0300

    Update inputrc_custom_bind_install.sh

--- inputrc_custom_bind_install.sh
+++ inputrc_custom_bind_install.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+PATH=/usr/local/bin:/usr/bin
+
 SCRIPTURL=https://github.com/zhubanRuban/cygwin-extras/raw/master/inputrc_custom_bind
 PATHTOINSTALL=/etc/profile.d/inputrc.sh
 
-echo Installing custom .inputrc via bind
+echo Installing custom .inputrc via bind https://github.com/zhubanRuban/cygwin-extras/blob/master/inputrc_custom_bind
 wget -nv --show-progress -O "$PATHTOINSTALL" "$SCRIPTURL"
